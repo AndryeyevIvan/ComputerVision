822c153a744e4e8a45f676f4be413a9b0cba8be6
Андрєєв Іван <ivanandryeyev@gmail.com>
2025-09-30 11:51:46 +0300

    Update HW_lesson3.py

--- HW_lesson3.py
+++ HW_lesson3.py
@@ -4,6 +4,6 @@ import numpy as np
 img = cv2.imread('images/VlasnePhoto.png')
 img = cv2.resize(img, (img.shape[1] * 2, img.shape[0] * 2))
 cv2.rectangle(img,(120,30),(336,350),(235, 73, 52), 1)
-cv2.putText(img, "Andryeyev Ivan", (105, 400), cv2.FONT_HERSHEY_PLAIN, 2, (235, 73, 52), 3)
+cv2.putText(img, "Andryeyev Ivan", (img.shape[1] // 2 - 65, 400), cv2.FONT_HERSHEY_PLAIN, 1, (235, 73, 52), 2)
 cv2.imshow('VlasnePhoto', img)
 cv2.waitKey(0)
